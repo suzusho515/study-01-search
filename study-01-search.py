4a243932842a139ca3ca2b0cd98b4097df0cddae
suzusho515 <89764444+suzusho515@users.noreply.github.com>
2021-08-31 00:42:54 +0900

    Add files via upload

---
+++ study-01-search.py
@@ -0,0 +1,21 @@
+import csv
+
+csv_path = r'C:\Users\sm051\Desktop\プログラミング\MENTA\課題\課題１\source.csv'
+
+def search():
+    word =input("鬼滅の登場人物の名前を入力してください >>> ")
+    with open(csv_path, encoding="utf-8") as f:
+      source = f.read().split('\n')
+
+      if word in source: #wordの照合成功
+        print(f"{word}が見つかりした")
+      else: #wordの照合失敗
+        print(f"{word}は見つかりませんでした")
+        source.append(word)
+        #書き込みモードでファイルを開く
+        with open(csv_path, mode = "w", newline="", encoding="utf-8") as f:
+          f.write("\n".join(source)) 
+
+
+if __name__ == "__main__":
+    search()
